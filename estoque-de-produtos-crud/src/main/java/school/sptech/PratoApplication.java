88a6f1cdc8f87e73e8d23689d8be6c9f34d934c2
m4yk0n <maykon.rodrigues@sptech.school>
2025-04-05 12:57:51 -0300

    fix: erros de compilação / add: swagger

--- estoque-de-produtos-crud/src/main/java/school/sptech/PratoApplication.java
+++ estoque-de-produtos-crud/src/main/java/school/sptech/PratoApplication.java
@@ -1,5 +1,8 @@
 package school.sptech;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class PratoApplication {
     public static void main(String[] args) {
 
--- estoque-de-produtos-crud/src/main/java/school/sptech/StatusPratoProdutoApplication.java
+++ estoque-de-produtos-crud/src/main/java/school/sptech/StatusPratoProdutoApplication.java
@@ -1,5 +1,8 @@
 package school.sptech;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class StatusPratoProdutoApplication {
     public static void main(String[] args) {
 
