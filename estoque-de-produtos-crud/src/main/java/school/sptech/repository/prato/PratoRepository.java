a79665d083ec81009309e16509d7465188ab0e0a
m4yk0n <maykon.rodrigues@sptech.school>
2025-04-06 19:44:31 -0300

    add: packages e classes para entidade prato

---
+++ estoque-de-produtos-crud/src/main/java/school/sptech/repository/prato/PratoRepository.java
@@ -0,0 +1,7 @@
+package school.sptech.repository.prato;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import school.sptech.entity.prato.Prato;
+
+public interface PratoRepository extends JpaRepository<Prato, Integer> {
+}
