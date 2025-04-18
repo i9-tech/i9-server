35be9b3754effa72282637d62709aa85e976b7bd
m4yk0n <maykon.rodrigues@sptech.school>
2025-04-18 15:09:13 -0300

    feat: operações entidade itemCarrinho

--- estoque-de-produtos-crud/src/main/java/school/sptech/controller/itemCarrinho/ItemCarrinhoController.java
+++ estoque-de-produtos-crud/src/main/java/school/sptech/controller/itemCarrinho/ItemCarrinhoController.java
@@ -1,8 +1,14 @@
 package school.sptech.controller.itemCarrinho;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import school.sptech.controller.itemCarrinho.dto.AdicionarPratoItemCarrinhoDto;
+import school.sptech.controller.itemCarrinho.dto.AdicionarProdutoItemCarrinhoDto;
+import school.sptech.controller.itemCarrinho.dto.ItemCarrinhoListagemDto;
+import school.sptech.controller.itemCarrinho.dto.ItemCarrinhoMapper;
 import school.sptech.service.itemCarrinho.ItemCarrinhoService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/itens-carrinho")
@@ -12,5 +18,43 @@ public class ItemCarrinhoController {
 
     public ItemCarrinhoController(ItemCarrinhoService itemCarrinhoService) {
         this.itemCarrinhoService = itemCarrinhoService;
+    }
+
+    @PostMapping("/prato")
+    public ResponseEntity<ItemCarrinhoListagemDto> adicionarPrato(
+            @RequestBody AdicionarPratoItemCarrinhoDto request
+            ) {
+        return ResponseEntity
+                .ok(ItemCarrinhoMapper.
+                        toPratoResponseDto(
+                                itemCarrinhoService.adicionarPrato(
+                                        ItemCarrinhoMapper.toEntity(request))));
+    }
+
+    @PostMapping("/produto")
+    public ResponseEntity<ItemCarrinhoListagemDto> adicionarProduto(
+            @RequestBody AdicionarProdutoItemCarrinhoDto request
+    ) {
+        return ResponseEntity
+                .ok(ItemCarrinhoMapper.
+                        toProdutoResponseDto(
+                                itemCarrinhoService.adicionarProduto(
+                                        ItemCarrinhoMapper.toEntity(request))));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ItemCarrinhoListagemDto>> listarItens(){
+        return ResponseEntity
+                .ok(ItemCarrinhoMapper.
+                        toResponseDtoList(
+                                itemCarrinhoService.listarItens()));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> removerItem(
+            @PathVariable Integer id
+    ) {
+        itemCarrinhoService.removerItem(id);
+        return ResponseEntity.ok().build();
     }
 }
--- estoque-de-produtos-crud/src/main/java/school/sptech/service/itemCarrinho/ItemCarrinhoService.java
+++ estoque-de-produtos-crud/src/main/java/school/sptech/service/itemCarrinho/ItemCarrinhoService.java
@@ -1,13 +1,34 @@
 package school.sptech.service.itemCarrinho;
 
 import org.springframework.stereotype.Service;
+import school.sptech.entity.itemCarrinho.ItemCarrinho;
 import school.sptech.repository.itemCarrinho.ItemCarrinhoRepository;
+
+import java.util.List;
 
 @Service
 public class ItemCarrinhoService {
 
     private final ItemCarrinhoRepository itemCarrinhoRepository;
 
+    public ItemCarrinho adicionarPrato(ItemCarrinho prato) {
+        prato.setProduto(null);
+        prato.setObservacao(null);
+        return itemCarrinhoRepository.save(prato);
+    }
+
+    public ItemCarrinho adicionarProduto(ItemCarrinho produto) {
+        produto.setPrato(null);
+        return itemCarrinhoRepository.save(produto);
+    }
+
+    public void removerItem(Integer id) {
+        itemCarrinhoRepository.deleteById(id);
+    }
+
+    public List<ItemCarrinho> listarItens() {
+        return itemCarrinhoRepository.findAll();
+    }
 
     public ItemCarrinhoService(ItemCarrinhoRepository itemCarrinhoRepository) {
         this.itemCarrinhoRepository = itemCarrinhoRepository;
