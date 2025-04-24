81058d7f05d6223ad11cf14a879043c9cddccfe0
Guilherme Borin Galeno <guilherme.galeno@sptech.school>
2025-04-24 09:39:28 -0300

    feat: @Query listagem por id

--- estoque-de-produtos-crud/src/main/java/school/sptech/repository/categoria/CategoriaRepository.java
+++ estoque-de-produtos-crud/src/main/java/school/sptech/repository/categoria/CategoriaRepository.java
@@ -13,6 +13,6 @@ public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
     @Query("select c from Categoria c join c.funcionario f where f.empresa = (select f2.empresa from Funcionario f2 where f2.id = :idFuncionario)")
     List<Categoria> buscarCategoriasDaEmpresaDoFuncionario(@Param("idFuncionario") Integer idFuncionario);
 
-
-
+    @Query("select c from Categoria c join c.funcionario f where c.id = :idCategoria and f.empresa = (select f2.empresa from Funcionario f2 where f2.id = :idFuncionario)")
+    Optional<Categoria> buscarCategoriaPorIdDoFuncionarioDaEmpresa(@Param("idCategoria") Integer idCategoria, @Param("idFuncionario") Integer idFuncionario);
 }
