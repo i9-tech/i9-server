99e4f8ced9365b2d0b92ece10bb0bc9241bc7f55
isabelarosalima <142421678+isabelarosalima@users.noreply.github.com>
2025-05-28 19:13:05 -0300

    feat: criando testes de cadastro e atualização

--- estoque-de-produtos-crud/src/test/java/school/sptech/service/funcionario/FuncionarioAtualizacaoTest.java
+++ estoque-de-produtos-crud/src/test/java/school/sptech/service/funcionario/FuncionarioAtualizacaoTest.java
@@ -1,10 +1,27 @@
 package school.sptech.service.funcionario;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
 public class FuncionarioAtualizacaoTest {
 
+    // ATUALIZANDO FUNCIONÁRIO COM DADOS VÁLIDOS
+    @Test
+    @DisplayName("Atualizar funcionário quando acionado com dados válidos deve retornar funcionário atualizada")
+    void atualizarFuncionarioQuandoAcionadoComDadosValidosDeveRetornarFuncionarioAtualizado(){
+
+    }
+
+    // LANÇANDO EXCEÇÃO QUANDO FUNCIONÁRIO COM DADOS INVÁLIDOS FOR ACIONADO
+    @Test
+    @DisplayName("Atualizar funcionário quando acionado com dados inválidos deve lançar Exceção")
+    void atualizarFuncionarioQuandoAcionadoComDadosInvalidosDeveRetornarExcecao(){
+
+    }
+
+    // ERRO AO TENTAR ATUALIZAR CPF E DATA DE ADMISSÃO
 
 }
--- estoque-de-produtos-crud/src/test/java/school/sptech/service/funcionario/FuncionarioCadastroTest.java
+++ estoque-de-produtos-crud/src/test/java/school/sptech/service/funcionario/FuncionarioCadastroTest.java
@@ -1,4 +1,47 @@
 package school.sptech.service.funcionario;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import school.sptech.repository.funcionario.FuncionarioRepository;
+
+@ExtendWith(MockitoExtension.class)
 public class FuncionarioCadastroTest {
+
+    @InjectMocks
+    FuncionarioService funcionarioService;
+
+    @Mock
+    FuncionarioRepository funcionarioRepository;
+
+
+    // CADASTRANDO FUNCIONÁRIO COM DADOS VÁLIDOS
+    @Test
+    @DisplayName("Cadastrar funcionário quando acionado com dados válidos deve retornar funcionário cadastrado")
+    void cadastrarFuncionarioQuandoAcionadoComDadosValidosDeveRetornarFuncionarioCadastrado(){
+
+
+
+    }
+
+    // RETORNANDO EXCEÇÃO QUANDO FUNCIONÁRIO COM DADOS INVÁLIDOS FOR CADASTRADO
+    @Test
+    @DisplayName("Cadastrar funcionário quando acionado com dados inválidos deve retornar exceção")
+    void cadastrarFuncionarioQuandoAcionadoComDadosInvalidosDeveRetornarExcecao(){
+
+
+    }
+
+    // ERRO AO CADASTRAR FUNCIONÁRIO COM EMPRESA INVÁLIDA
+    @Test
+    @DisplayName("Cadastrar funcionário quando acionado com empresa inválida deve retornar EntidadeNaoEncontradaException/")
+    void cadastrarFuncionarioQuandoAcionadoComEmpresaInvalidaDeveRetornarExcecao(){
+
+
+    }
+
+
 }
