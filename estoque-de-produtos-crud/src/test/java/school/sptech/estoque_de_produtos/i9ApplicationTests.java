08a1688fecfa06304ae6e50c25c6e000ef4e766e
m4yk0n <maykon.rodrigues@sptech.school>
2025-06-12 17:59:54 -0300

    add: novo properties para testes

--- estoque-de-produtos-crud/src/test/java/school/sptech/estoque_de_produtos/i9ApplicationTests.java
+++ estoque-de-produtos-crud/src/test/java/school/sptech/estoque_de_produtos/i9ApplicationTests.java
@@ -2,8 +2,10 @@ package school.sptech.estoque_de_produtos;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class i9ApplicationTests {
 
 	@Test
